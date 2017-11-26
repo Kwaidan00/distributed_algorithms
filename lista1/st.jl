d572f305a2e0fe6db44dd3ca6ac0e1f1b93ef785
Kwaidan00 <aleksander.spyra@gmail.com>
2017-11-26 18:31:09 +0100

    The first assignment

---
+++ lista1/st.jl
@@ -0,0 +1,140 @@
+# Algorytmy rozproszone
+# Lista 1
+
+# Aleksander Spyra
+
+# MODEL
+# Każdy wierzchołek to procesor z unikalnym UID
+# parent = 0
+# children = 0
+#
+# root (i0)
+# root sends its ID to N(i0)
+
+# Rest of nodes:
+# 1. if w received message for the first time from v
+#    a) it sends "you are my father" to v
+#    b) set parent <- v
+#    c) send message to N(w)
+# 2. if w received "you are my parent" from v
+#    a) chilren = children \cup {w}
+
+type Node
+  uid::Int64
+
+  leader::Bool
+  neighbourhood::Set{Int64}
+  parent::Int64
+  childrens::Array{Int64}
+
+  rootFunction::Function
+  nodeFunction::Function
+  setLeader::Function
+  addToNeighbourhood::Function
+
+
+  function Node(id::Int64)
+    this = new()
+    this.uid = id
+    this.leader = false
+    this.parent = 0
+    this.childrens = []
+    this.neighbourhood = Set{Int64}()
+
+    this.setLeader = function ()
+      leader = true
+    end
+    this.rootFunction = function (channels::Array{Channel{Tuple{Symbol, Int}}})
+      # root wysyła swoje ID to wszystkich swoich sąsiadów
+      if this.leader
+        for id in this.neighbourhood
+          put!(channels[id], (:search, this.uid))
+        end
+        rMsg = 0
+        eMsg = length(this.neighbourhood)
+        while rMsg < eMsg
+          (msg, senderId) = take!(channels[this.uid])
+          rMsg += 1
+          if msg == :youaremyfather
+            push!(childrens, senderId)
+          elseif msg == :search
+            put!(channels[senderId], (:goaway, this.uid))
+          end
+        end #while
+
+      end
+    end
+    this.nodeFunction = function (channels::Array{Channel{Tuple{Symbol, Int}}})
+      if !this.leader
+        rMsg = 0
+        eMsg = length(this.neighbourhood)
+        while rMsg < eMsg
+          (msg, senderId) = take!(channels[this.uid])
+          rMsg += 1
+          if msg == :search
+            if this.parent == 0
+              parent = senderId
+              put!(channels[senderId], (:youaremyfather, this.uid))
+              for id in this.neighbourhood
+                if id != senderId
+                  put!(channels[id], (:search, this.uid))
+                end
+              end
+            else
+              put!(channels[senderId], :goaway)
+            end
+          elseif msg == :youaremyfather
+            push!(this.childrens, senderId)
+          end
+        end # while
+      end
+    end
+    this.addToNeighbourhood = function (neighbour::Int64)
+      push!(this.neighbourhood, neighbour)
+    end
+    return this
+  end
+
+end
+
+
+function st(nodes ::Node[], leaderIndex ::Int64 = 1)
+  maxRound = 1000
+  for i in 1:maxRound
+    # decide if I want to send sth
+  end
+
+end
+
+n = 5
+testRange = 1:n
+nodes = []
+for i in testRange
+  push!(nodes, Node(i))
+end
+
+#     1 - 2 - 4
+#      \  |   |
+#       3 --- 5
+nodes[1].addToNeighbourhood(2)
+nodes[1].addToNeighbourhood(3)
+nodes[2].addToNeighbourhood(1)
+nodes[2].addToNeighbourhood(3)
+nodes[2].addToNeighbourhood(4)
+nodes[3].addToNeighbourhood(1)
+nodes[3].addToNeighbourhood(2)
+nodes[3].addToNeighbourhood(5)
+nodes[4].addToNeighbourhood(2)
+nodes[4].addToNeighbourhood(5)
+nodes[5].addToNeighbourhood(3)
+nodes[5].addToNeighbourhood(4)
+
+channels = [Channel{Tuple{Symbol, Int}}(n) for i=1:n]
+
+@sync for i in 1:n
+  if nodes[i].leader
+    @async nodes[i].rootFunction(channels)
+  else
+    @async nodes[i].nodeFunction(channels)
+  end
+end
